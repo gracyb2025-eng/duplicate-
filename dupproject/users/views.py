c67ee5d8450e5e232063cc554723519c08fe8450
Bawuza Grace <gracyb2025@gmail.com>
2026-05-27 14:37:54 +0300

    Worked on authentication and registeriing required users

--- dupproject/users/views.py
+++ dupproject/users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+
 from .forms import RegisterForm
 from .models import UserProfile
 
@@ -16,7 +17,6 @@ def register_user(request):
 
             user = User.objects.create_user(
                 username=form.cleaned_data['username'],
-            
                 password=form.cleaned_data['password']
             )
 
@@ -37,6 +37,7 @@ def register_user(request):
         'registration/register.html',
         {'form': form}
     )
+
 
 def login_user(request):
 
@@ -70,6 +71,7 @@ def login_user(request):
             messages.error(request, "Invalid username or password")
 
     return render(request, 'registration/login.html')
+
 
 def logout_user(request):
 
